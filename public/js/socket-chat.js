56753b42d5e0362ae3b997921ba5fff8a0ea6e4e
juan-gon <agonzales@peruyoung.com>
2020-09-09 16:24:34 +0200

    ventanas html

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,7 +1,7 @@
 var socket = io();
-//
+
 var params = new URLSearchParams(window.location.search);
-console.log(params.has('nombre'));
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
@@ -10,37 +10,51 @@ if (!params.has('nombre') || !params.has('sala')) {
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
-    console.log('conectado al servidor');
+    console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
-//escucha
+
+// escuchar
 socket.on('disconnect', function() {
-    console.log('Conexion fallida')
-});
-socket.on('enviarMensaje', (message) => {
-    console.log(message);
-});
-//emite
-socket.emit('enviarMensaje', {
-    usuario: 'Angel',
-    mensaje: 'Hola mundo'
-}, function(resp) {
-    console.log('Respuesta Servidor', resp);
+
+    console.log('Perdimos conexión con el servidor');
+
 });
 
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje Privado: ', mensaje)
-})
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
